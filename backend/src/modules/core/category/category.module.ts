1561859135a48353da6ee0185c18455542d5ed77
nbinayak02 <binayak0128@gmail.com>
2026-06-04 08:59:09 +0545

    feat: category module

--- backend/src/modules/core/category/category.module.ts
+++ backend/src/modules/core/category/category.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
+import { PrismaService } from '@shared/prisma';
 
 @Module({
   controllers: [CategoryController],
-  providers: [CategoryService]
+  providers: [CategoryService, PrismaService],
 })
 export class CategoryModule {}
