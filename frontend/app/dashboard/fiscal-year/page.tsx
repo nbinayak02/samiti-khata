ca64ce1c1b87c31ecb735bdd985d197b605fc192
Binayak Niraula <binayak0128@gmail.com>
2026-07-16 07:43:50 +0545

    feat: fiscal year table

--- frontend/app/dashboard/fiscal-year/page.tsx
+++ frontend/app/dashboard/fiscal-year/page.tsx
@@ -1,7 +1,22 @@
-import { PageHeader, PageHeading, PageLayout } from "@/components/pages";
+import {
+  PageHeader,
+  PageHeading,
+  PageLayout,
+  PageTable,
+} from "@/components/pages";
+import { getFiscalYears } from "@/features/fiscal-year/api/fiscalYear.client.api";
 import { AddFiscalYearDialog } from "@/features/fiscal-year/components/Add-FiscalYear-Dialog";
+import FiscalYearTable from "@/features/fiscal-year/components/FiscalYear-Table";
+import { getQueryClient } from "@/lib/query/query-client";
+import { QUERY_KEYS } from "@/lib/query/query-keys";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-export default function FiscalYearPage() {
+export default async function FiscalYearPage() {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: [QUERY_KEYS.FISCAL_YEAR],
+    queryFn: getFiscalYears,
+  });
   return (
     <PageLayout>
       <PageHeader>
@@ -11,6 +26,11 @@ export default function FiscalYearPage() {
         />
         <AddFiscalYearDialog />
       </PageHeader>
+      <PageTable>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <FiscalYearTable />
+        </HydrationBoundary>
+      </PageTable>
     </PageLayout>
   );
 }
